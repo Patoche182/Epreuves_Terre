834ed8ecee85d7639bdc5bcfb5fee69cab85f88e
PatriceHANNE <patrice.hanne@gmail.com>
2023-12-13 00:41:02 +0100

    Commit 3

--- CodingAccelerator-Terre/Program.cs
+++ CodingAccelerator-Terre/Program.cs
@@ -1,25 +1,25 @@
-﻿//class Terre00
-//{
-//    static void Main()
-//    {
-//        string lettreAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//        foreach (char lettre in lettreAlphabet)
-//        {
-//            Console.Write(lettre.ToString().ToLower());
-//        }
-//        Console.WriteLine();
-//    }
-//}
-
-class Terre00
+﻿class Terre00
 {
     static void Main()
     {
-        for (char lettre = 'A'; lettre <= 'Z'; lettre++)
-
+        string lettreAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        foreach (char lettre in lettreAlphabet)
         {
-            Console.Write(lettre.ToString().ToLower() + " ");
+            Console.Write(lettre.ToString().ToLower());
         }
         Console.WriteLine();
     }
 }
+
+//class Terre00
+//{
+//    static void Main()
+//    {
+//        for (char lettre = 'A'; lettre <= 'Z'; lettre++)
+
+//        {
+//            Console.Write(lettre.ToString().ToLower() + " ");
+//        }
+//        Console.WriteLine();
+//    }
+//}
