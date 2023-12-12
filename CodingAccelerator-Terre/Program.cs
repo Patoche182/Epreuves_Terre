3ccde746b76a8e2e726f3b89651c974889a047bf
PatriceHANNE <patrice.hanne@gmail.com>
2023-12-13 00:33:18 +0100

    Ajoutez des fichiers projet.

---
+++ CodingAccelerator-Terre/Program.cs
@@ -0,0 +1,12 @@
+﻿class Terre00
+{
+    static void Main()
+    {
+        string lettreAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        foreach (char lettre in lettreAlphabet)
+        {
+            Console.Write(lettre.ToString().ToLower());
+        }
+        Console.WriteLine();
+    }
+}
